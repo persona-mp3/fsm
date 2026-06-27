77e34bcb1ac014b6fe80699c875d9d1fa12a457c
persona-mp3 <randomnobscurebs@gmail.com>
2026-06-27 12:09:35 +0100

    feat: added tracing and profiling

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"context"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"runtime/trace"
 )
 
 func main() {
-	const stubTotalNodes = 3
+	const stubTotalNodes = 4
 	cluster := NewCluster(stubTotalNodes, nil)
 
+	go func() {
+		f, _ := os.Create("trace.out")
+		trace.Start(f)
+		defer trace.Stop()
+
+		if err := http.ListenAndServe("localhost:6061", nil); err != nil {
+			log.Panicln("could not run tacer::", err)
+		}
+
+	}()
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
 	cluster.Start(ctx)
