3f2581ceb39dfe5fbd3cd21efe813605a263807f
persona-mp3 <randomnobscurebs@gmail.com>
2026-06-27 01:59:13 +0100

    feat: scafolded sending heartbeat loop for leader

--- leader.go
+++ leader.go
@@ -1,17 +1,63 @@
 package main
 
-import "fsm/raftlogger"
+import (
+	"fmt"
+	"sync"
+	"context"
+	rlog "fsm/raftlogger"
+	"net/rpc"
+	"time"
+)
 
-func (n *Node) runLeader(logger raftlogger.RLogger) {
+func (n *Node) runLeader(logger rlog.RLogger) {
 	logger.Println("leader state transitioned successfully", n.Diagnostics())
+	ticker := time.NewTicker(heartbeatInterval)
 	defer func() {
+		ticker.Stop()
 		logger.Println("leader state terminated succesfully")
+	}()
+
+	ctx, cancel := context.WithCancel(n.stateCtx)
+	defer cancel()
+
+	wg := sync.WaitGroup{}
+
+	if len(n.connectedPeers) == 0 {
+		// we might want a resting state or Unit because it could possbily 
+		// mean this node is the only one active in the cluster and others have died
+		logger.Println("todo::warning:: could not find any connected peer")
+	}
+
+	for idx,  rpcPeer := range n.connectedPeers  {
+		if rpcPeer != nil {
+			wg.Add(1)
+
+			childLogger := n.log.Inherit(fmt.Sprintf("%d-sendHB", idx))
+
+			go func(ctx context.Context, d *rpc.Client, logger rlog.RLogger){
+				defer wg.Done()
+				sendHeartBeat(ctx, d,  heartbeatInterval, logger)
+			}(ctx, rpcPeer, childLogger)
+		}
+	}
+
+	// wait for all children to return
+	done := make(chan struct{})
+	go func(){
+		wg.Wait()
+		done <- struct{}{}
 	}()
 
 	for {
 		select {
 		case <-n.stateCtx.Done():
 			return
+
+		case <-done:
+			logger.Println("all child sendhearbeats have returned")
+			n.transition <- Follower
+			return
+
 		case req := <-n.incoming:
 			switch req.kind {
 			case AppendEntry:
@@ -34,6 +80,26 @@ func (n *Node) runLeader(logger raftlogger.RLogger) {
 			default:
 				logger.Panic("Unhandled RPC Not yet implemented:", req.payload, n.Diagnostics())
 			}
+		}
+	}
+}
+
+func sendHeartBeat(ctx context.Context, dial *rpc.Client, interval time.Duration, logger rlog.RLogger) {
+	ticker := time.NewTicker(interval)
+	defer func(){
+		ticker.Stop()
+		logger.Println("returning back to parent")
+	}()
+
+	for {
+		select {
+		case <-ticker.C:
+			logger.Println("sending heartbeatRPC")
+			time.Sleep(2  * time.Second)
+			ticker.Reset(interval)
+
+		case <-ctx.Done():
+			return
 		}
 	}
 }
